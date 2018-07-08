41212508281540d6b2ac105a9913460a8a640efa
Daniel <daniel.tooke@gmail.com>
2018-07-08 17:25:40 +0100

    Added ability for docking station to dock bikes

--- docking_station.py
+++ docking_station.py
@@ -2,3 +2,6 @@ class DockingStation:
 
     def __init__(self):
         self.rack = []
+
+    def dock_bike(self, bike):
+        self.rack.append(bike)
--- docking_station_test.py
+++ docking_station_test.py
@@ -6,5 +6,11 @@ class TestDockingStation(unittest.TestCase):
     def test_rack(self):
         self.assertIsInstance( DockingStation().rack, list)
 
+    def test_dock_bike(self):
+        bike = 'bike'
+        station = DockingStation()
+        station.dock_bike(bike)
+        self.assertTrue( station.rack[0] == bike)
+
 if __name__ == '__main__':
     unittest.main()
