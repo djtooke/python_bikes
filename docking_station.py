3452e9e38677b6c717ae1a590c3f590f1b22cdcc
Daniel Tooke <daniel.tooke@gmail.com>
2018-07-18 16:09:24 +0100

    Added ability to set capacity of docking stations

--- docking_station.py
+++ docking_station.py
@@ -2,8 +2,9 @@ class DockingStation:
 
     DEFAULT_CAPACITY = 20
 
-    def __init__(self):
+    def __init__(self, capacity = DEFAULT_CAPACITY):
         self.rack = []
+        self.capacity = capacity
 
     def dock_bike(self, bike):
         self.rack.append(bike)
--- docking_station_test.py
+++ docking_station_test.py
@@ -12,8 +12,15 @@ class TestDockingStation(unittest.TestCase):
         station.dock_bike(bike)
         self.assertTrue( station.rack[0] == bike)
 
-    def test_default_capacity(self):
+    def test_default_capacity_constant(self):
         self.assertEqual( DockingStation().DEFAULT_CAPACITY, 20)
+
+    def test_default_capacity_set(self):
+        self.assertEqual( DockingStation().capacity, 20)
+
+    def test_setting_chosen_capacity(self):
+        station = DockingStation(15)
+        self.assertEqual( station.capacity, 15)
 
 if __name__ == '__main__':
     unittest.main()
